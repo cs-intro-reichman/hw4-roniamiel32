430bdd5e6af708db2d17cd1366910994a95d6417
Roni <roni.amiel@post.runi.ac.il>
2024-12-04 21:47:14 +0200

    Completed the mission

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -183,21 +183,27 @@ public class ArrCharOps {
      *         return -2 if there is an error with the input.
      */
     public static int compareTo(String str1, String str2) {
-        // Validate input strings
+        // Handle invalid inputs (e.g., null strings)
         if (str1 == null || str2 == null) {
-            return -2; // Indicate invalid input
+            System.out.println("One or both strings are null.");
+            return -2; // Special value indicating an error
         }
 
-        if (str1.length() > str2.length())
-            return 1;
-        if (str1.length() < str2.length())
-            return -1;
+        int minLength = Math.min(str1.length(), str2.length());
 
-        for (int i = 0; i < str1.length(); i++) {
-            if (str1.charAt(i) < str2.charAt(i))
-                return -1;
-            if (str1.charAt(i) > str2.charAt(i))
-                return 1;
+        for (int i = 0; i < minLength; i++) {
+            if (str1.charAt(i) < str2.charAt(i)) {
+                return -1; // str1 is smaller
+            } else if (str1.charAt(i) > str2.charAt(i)) {
+                return 1; // str1 is larger
+            }
+        }
+
+        // If all characters match, compare lengths
+        if (str1.length() < str2.length()) {
+            return -1;
+        } else if (str1.length() > str2.length()) {
+            return 1;
         }
 
         return 0; // Strings are equal
